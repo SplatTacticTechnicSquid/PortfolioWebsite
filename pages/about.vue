b126806b34fa5467318be98f9eb2e0148d3a86dc
Raditya Pratama <radityaprtm2002@gmail.com>
2025-02-10 14:53:17 +0700

    Added container in about me page profile to align with navbar

--- pages/about.vue
+++ pages/about.vue
@@ -1,9 +1,14 @@
 <template>
-  <div class="p-10 text-center">
-    <h2 class="text-4xl font-bold text-white">About Me</h2>
-    <p class="text-gray-300 mt-4">
-      I'm a full-stack developer with experience in Vue, Nuxt, and backend APIs.
+  <div class="max-w-6xl mx-auto px-6 py-10">
+    <h1 class="text-4xl font-bold text-center">About Me</h1>
+    <p class="text-center text-gray-300 mt-4">
+      I'm a full-stack developer with experience in Vue.js, Nuxt.js, Next.js,
+      and backend APIs.
     </p>
+
+    <div class="mt-10 pl-0 md:pl-2.5 lg:pl-4">
+      <p class="text-white font-mono text-lg">> $ Whoami</p>
+    </div>
   </div>
 </template>
 
