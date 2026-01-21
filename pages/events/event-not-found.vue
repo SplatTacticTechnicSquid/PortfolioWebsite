46c1af5c58902a1620e6b3899d9a8e1be410ed42
radityaprtm <radityaprtm2002@gmail.com>
2026-01-21 20:07:49 +0700

    feat(eventwarn): added a file that tells the user if tries to access a non existing event

---
+++ pages/events/event-not-found.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="text-center py-20 text-white">
+    <h1 class="text-5xl font-bold text-red-500">Event Not Found</h1>
+    <p class="text-lg text-gray-400 mt-4">
+      Oops! We couldn't find the event you're looking for.
+    </p>
+
+    <NuxtLink
+      to="/events"
+      class="mt-6 inline-block px-6 py-3 bg-blue-500 hover:bg-blue-600 rounded-lg"
+    >
+      Back to Events
+    </NuxtLink>
+  </div>
+</template>
