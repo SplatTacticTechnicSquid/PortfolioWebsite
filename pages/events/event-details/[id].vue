f44e26dc81f3c7df34262df285ea1450686d589e
radityaprtm <radityaprtm2002@gmail.com>
2026-01-21 20:08:19 +0700

    feat(id): added dynamic routing for events page using nuxt id

---
+++ pages/events/event-details/[id].vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="p-10">
+    <Transition name="fade" mode="out-in">
+      <component :is="EventComponent" :key="route.params.id" />
+    </Transition>
+  </div>
+</template>
+
+<script setup>
+import { defineAsyncComponent, computed } from "vue";
+import { useRoute } from "vue-router";
+import EventNotFound from "../event-not-found.vue";
+
+const route = useRoute();
+const eventId = route.params.id;
+
+const eventComponents = {
+  "nuxt-portfolio": defineAsyncComponent(() => import("./nuxt-portfolio.vue")),
+
+  "sigma-mengajar": defineAsyncComponent(() => import("./sigma-mengajar.vue")),
+  // add more events here
+};
+
+const EventComponent = computed(
+  () => eventComponents[eventId] || EventNotFound,
+);
+</script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease-in-out;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
